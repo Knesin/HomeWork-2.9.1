42e519181950d89278971dc83364295a505a95a7
Maksim Kl <GuruHacker@yandex.ru>
2023-03-01 22:40:39 +0300

    Initial commit

---
+++ HomeWork-2.9.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Fraction.h"
+
+int main()
+{
+	Fraction f1(4, 3);//1.(3)
+	Fraction f2(6, 11);//0.(54)
+	//Fraction f2(8, 6);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}  
